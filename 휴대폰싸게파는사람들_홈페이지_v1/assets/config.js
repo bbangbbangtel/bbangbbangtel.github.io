b96daaeae05699d6d1fd2bc5f2e8b55971aa8a12
bbangbbangtel <gsb2526@gmail.com>
2026-08-13 00:04:40 +0900

    Add files via upload

---
+++ 휴대폰싸게파는사람들_홈페이지_v1/assets/config.js
@@ -0,0 +1,9 @@
+// 대표님이 나중에 바꾸기 쉬운 기본 정보
+window.BUSINESS_CONFIG = {
+  businessName: "휴대폰싸게파는사람들",
+  phone: "전화번호 입력",
+  kakaoUrl: "#",
+  address: "광주광역시 광산구 장신로 134 1층",
+  hours: "10:30 ~ 20:30",
+  parking: "놀부부대찌개 옆 지하주차장 이용"
+};
