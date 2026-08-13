0a5202a12c89a462d717bdc19a85ce248ce3ac21
bbangbbangtel <gsb2526@gmail.com>
2026-08-13 23:57:51 +0900

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,9 +1,11 @@
-// 대표님이 나중에 바꾸기 쉬운 기본 정보
+// 홈페이지 공통 사업 정보
 window.BUSINESS_CONFIG = {
   businessName: "휴대폰싸게파는사람들",
-  phone: "전화번호 입력",
-  kakaoUrl: "#",
-  address: "광주광역시 광산구 장신로 134 1층",
+  storeName: "공구폰",
+  phone: "010-4191-1918",
+  phoneHref: "tel:01041911918",
+  kakaoUrl: "https://pf.kakao.com/_nQXxdn",
+  address: "광주광역시 광산구 장신로 134 1층 106호",
   hours: "10:30 ~ 20:30",
   parking: "놀부부대찌개 옆 지하주차장 이용"
 };
