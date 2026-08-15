83dff03098e7f2abea0cf5ea0e4fb0d9ae976a88
bbangbbangtel <gsb2526@gmail.com>
2026-08-15 14:30:49 +0900

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,11 +1,31 @@
 // 홈페이지 공통 사업 정보
 window.BUSINESS_CONFIG = {
+
+  // 공식 상호 / 브랜드
   businessName: "휴대폰싸게파는사람들",
-  storeName: "공구폰",
+
+  // 기존 코드 호환용 매장명
+  storeName: "휴대폰싸게파는사람들",
+
+  // 실제 오프라인 매장에서 보이는 간판명
+  signName: "공구폰",
+
+  // 상담 연락처
   phone: "010-4191-1918",
   phoneHref: "tel:01041911918",
+
+  // 카카오채널
   kakaoUrl: "https://pf.kakao.com/_nQXxdn",
+
+  // 매장정보
   address: "광주광역시 광산구 장신로 134 1층 106호",
   hours: "10:30 ~ 20:30",
-  parking: "놀부부대찌개 옆 지하주차장 이용"
+  parking: "놀부부대찌개 옆 지하주차장 이용",
+
+  // 공식 홈페이지
+  websiteUrl: "https://bbangbbangtel.github.io/",
+
+  // 대표 공유 이미지
+  ogImage: "https://bbangbbangtel.github.io/assets/og-image.png"
+
 };
